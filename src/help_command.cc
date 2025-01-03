a6909636ca01d6492ce6db7f9c11b3b6d9e88f60
bramtechs <bramtech@telenet.be>
2025-01-03 17:00:06 +0100

    typos

--- src/help_command.cc
+++ src/help_command.cc
@@ -7,8 +7,8 @@ namespace sqlite_todo {
 bool HelpCommand::run(const std::string& param)
 {
     std::cout << "add <task title> - Adds a todo task." << '\n';
-    std::cout << "complete <id> - Marks a task as completed by removing it:" << '\n';
-    std::cout << "list - Lists all the tasks that haven't been done:" << '\n';
+    std::cout << "complete <id> - Marks a task as completed by removing it." << '\n';
+    std::cout << "list - Lists all the tasks that haven't been done." << '\n';
     std::cout << "clear - Removes the tasks database" << '\n';
     std::cout << "help - Shows this help screen" << '\n';
 
