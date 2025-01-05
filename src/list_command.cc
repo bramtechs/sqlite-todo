4eb90186d62fbe7bf955a436ab972520d0703a8e
bramtechs <bramtech@telenet.be>
2025-01-05 16:48:58 +0100

    list command: show message when there are no entries

--- src/list_command.cc
+++ src/list_command.cc
@@ -10,6 +10,9 @@ bool ListCommand::run(const std::string&)
     TodoStore& store = TodoStore::getInstance();
 
     auto items = store.getItems();
+    if (!items.hasNext()) {
+        std::cout << "There are no TODO items yet, add them with the 'add' command!\n";
+    }
     while (items.hasNext()) {
         std::cout << items.next() << "\n";
     }
